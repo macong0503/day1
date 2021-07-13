b47b4d5ffe70fce211875b77eb3a44d693126881
macong0503 <1512346520@qq.com>
2021-07-13 15:02:14 +0800

    Create Demo2.java

---
+++ Demo2.java
@@ -0,0 +1,13 @@
+public class Demo2 {
+    public static void main(String[] args) {
+
+        System.out.println(0);//整数常量
+        System.out.println(0.1);//小数常量
+
+        System.out.println("aaa");//字符串常量
+        System.out.println('a');//字符常量
+        System.out.println(true);//Boolean常量
+        //null不能直接输出
+        //System.out.println(null);//空常量
+    }
+}
