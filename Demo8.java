54f3604e14b0738043ff1b6aa292a518895126b3
macong0503 <1512346520@qq.com>
2021-07-13 15:02:39 +0800

    Create Demo8.java

---
+++ Demo8.java
@@ -0,0 +1,20 @@
+public class Demo8 {
+    public static void main(String[] args) {
+        //赋值运算
+        /*
+        * */
+        //+=
+        int i=1;
+        i+=1;//i=i+1,把计算结果的数据类型转换为+=左边那个变量的数据类型
+        byte b =1;
+//        byte c=b+1;
+        b+=1;
+        //-=
+        int j=2;
+        System.out.println(j-=1);
+        //*=
+        int k=1;
+        System.out.println(k*=6);
+
+    }
+}
