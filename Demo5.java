31d06db36dd0a7ed5ccf45d2d4cbbaf69915f6af
macong0503 <1512346520@qq.com>
2021-07-13 15:02:23 +0800

    Create Demo5.java

---
+++ Demo5.java
@@ -0,0 +1,19 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        //关系运算符
+        int i = 1;
+        int j = 2;
+        //等于 ==
+        boolean b1 = i == j;
+        System.out.println(b1);
+        //不等于 !=
+        boolean b2 = i != j;
+        System.out.println(b2);
+        //大于 >
+        boolean b3 = i > j;
+        System.out.println(b3);
+        //小于 <
+        boolean b4 = i < j;
+        System.out.println(b4);
+    }
+}
