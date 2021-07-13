5ec694d412137249d5c12a07c2fe6e248d8a11cb
macong0503 <1512346520@qq.com>
2021-07-13 15:02:20 +0800

    Create Demo4.java

---
+++ Demo4.java
@@ -0,0 +1,26 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        //算数运算符
+        int a = 21;
+        int b = 10;
+        //取余
+        int c = a % b;
+        //除法
+        int d = a / b;
+        System.out.println(c);
+        System.out.println(d);
+        // ++ --
+        int i = 1;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println("---------");
+        System.out.println(++i);
+        System.out.println(i);
+        System.out.println("=========");
+        int j = 5;
+        System.out.println(j--);
+        System.out.println(j);
+        System.out.println(--j);
+        System.out.println(j);
+    }
+}
