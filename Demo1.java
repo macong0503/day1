23b44b8f58dff3f17ec108cbfacafc1fb5673589
macong0503 <1512346520@qq.com>
2021-07-13 15:02:11 +0800

    Create Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+public class Demo1 {
+    //这是一个测试java注释的类
+    //java程序入口方法main
+    /*
+     * 这是一个多行注释
+     * */
+    public static void main(String[] args) {
+        //这是一个输出语句
+        System.out.println("这是一个测试java注释的输出");
+        //定义一个年龄为22
+        int age = 22;//见文知意
+    }
+}
+
